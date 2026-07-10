b35e676e361b1095dea4cb4e22c3a182a0837cf6
Hassaan Ahmed <murtrax2@gmail.com>
2026-07-10 10:20:32 +0500

    add vercel speed insights  pkg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "@/components/ui/Header";
 import "./globals.css";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
